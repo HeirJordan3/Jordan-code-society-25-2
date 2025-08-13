0b2361d0a44fd95b61f0f4d585594aea58a6f34e
HeirJordan3 <Jordan@codedifferently.com>
2025-08-13 21:39:03 +0000

    feat: added brooklyns changes and fixed merge conflicts

--- lesson_03/quiz/src/quizzes/quizzes.module.ts
+++ lesson_03/quiz/src/quizzes/quizzes.module.ts
@@ -3,11 +3,23 @@ import { AnotherQuiz } from './another_quiz.js';
 import { AnthonyMaysQuiz } from './anthony_mays_quiz.js';
 import { JordanEldridgeQuiz } from './jordan_eldridge.js';
 import { BrooklynHardenQuiz } from './brooklyn_harden_quiz.js';
+import { JordanEldridgeQuiz } from './jordan_eldridge.js';
 export const Quizzes = Symbol.for('Quizzes');
 
 // Add your quiz provider here.
 
+<<<<<<< HEAD
 const QUIZ_PROVIDERS = [AnthonyMaysQuiz, AnotherQuiz, BrooklynHardenQuiz,JordanEldridgeQuiz];
+=======
+
+const QUIZ_PROVIDERS = [
+  AnthonyMaysQuiz,
+  AnotherQuiz,
+  BrooklynHardenQuiz,
+  JordanEldridgeQuiz
+
+];
+>>>>>>> 774e821 (feat: added brooklyns changes and fixed merge conflicts)
 
 @Module({
   providers: [
